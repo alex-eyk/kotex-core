3a9ed80272ddf652a05b275bab3bd5448f836c43
Alexey Kiselev <ximand931@gmail.com>
2023-01-15 17:02:58 +0300

    Arguments extensions

---
+++ src/main/kotlin/com/alex/eyk/kotex/latex/ArgumentsExt.kt
@@ -0,0 +1,56 @@
+package com.alex.eyk.kotex.latex
+
+import com.alex.eyk.kotex.util.removeLast
+
+internal fun String.withBracesOrEmpty(): String {
+    return if (this.isEmpty()) "" else "{$this}"
+}
+
+internal fun String.asOption(): String {
+    return if (this.isEmpty()) "" else "[$this]"
+}
+
+internal fun Int.asOption(): String {
+    return "[$this]"
+}
+
+internal fun List<String>.asOptionsString(): String {
+    if (this.isEmpty()) {
+        return ""
+    }
+    val optionsBuilder = StringBuilder("[")
+    this.forEach {
+        optionsBuilder.append(it)
+            .append(", ")
+    }
+    optionsBuilder.removeLast(offset = 2)
+    return "$optionsBuilder]"
+}
+
+internal fun List<String>.asAdditionalOptionsString(): String {
+    return this.wrapEachWith(
+        start = "[",
+        end = "]"
+    )
+}
+
+internal fun List<String>.asAdditionalArgumentsString(): String {
+    return this.wrapEachWith(
+        start = "{",
+        end = "}"
+    )
+}
+
+private fun List<String>.wrapEachWith(
+    start: String,
+    end: String
+): String {
+    if (this.isEmpty()) {
+        return ""
+    }
+    val wrappedBuilder = StringBuilder()
+    this.forEach {
+        wrappedBuilder.append("$start$it$end")
+    }
+    return wrappedBuilder.toString()
+}
---
+++ src/main/kotlin/com/alex/eyk/kotex/util/StringBuilderExt.kt
@@ -0,0 +1,48 @@
+package com.alex.eyk.kotex.util
+
+private const val START_INDEX = 0
+
+private const val OPEN_BRACE = "{"
+private const val CLOSE_BRACE = "}"
+
+internal fun StringBuilder.removeLast(
+    offset: Int
+): StringBuilder = apply {
+    this.delete(
+        this.length - offset,
+        this.length
+    )
+}
+
+internal fun StringBuilder.insert(
+    content: String
+): StringBuilder = apply {
+    this.insert(
+        START_INDEX,
+        content
+    )
+}
+
+internal fun StringBuilder.wrapWithBrace(): StringBuilder = apply {
+    this.wrapWith(
+        start = OPEN_BRACE,
+        end = CLOSE_BRACE
+    )
+}
+
+internal fun StringBuilder.wrapWith(
+    start: String,
+    end: String
+) = apply {
+    this.insert(
+        START_INDEX,
+        start
+    )
+    this.append(end)
+}
+
+internal fun StringBuilder.breakLine(): StringBuilder = apply {
+    this.append(
+        System.lineSeparator()
+    )
+}
