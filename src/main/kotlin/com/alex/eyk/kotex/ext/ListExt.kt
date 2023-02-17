79745131ed1c0e6d4a44aa6ba6a86a51534fe068
Alexey Kiselev <ximand931@gmail.com>
2023-02-17 11:19:51 +0300

    list ext

--- src/main/kotlin/com/alex/eyk/kotex/ext/ListExt.kt
+++ src/main/kotlin/com/alex/eyk/kotex/ext/ListExt.kt
@@ -1,5 +1,16 @@
 package com.alex.eyk.kotex.ext
 
+fun <T> listOf(
+    size: Int,
+    init: (i: Int) -> T
+) : List<T> {
+    return ArrayList<T>().apply {
+        for (i in 0 until size) {
+            add(init(i))
+        }
+    }
+}
+
 fun <E> MutableList<E>.insert(
     item: E
 ) {
@@ -16,4 +27,8 @@ fun <E> List<E>.isLast(
     i: Int
 ): Boolean {
     return i == (this.size - 1)
+}
+
+fun <E> List<E>.copy(): MutableList<E> {
+    return ArrayList(this)
 }
